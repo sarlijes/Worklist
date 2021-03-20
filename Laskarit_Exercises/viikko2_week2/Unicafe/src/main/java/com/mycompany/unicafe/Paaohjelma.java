93e1da1fe35964e8a4d6564375d247426d4adee3
Jessica Sarlin <jessica.sarlin@gmail.com>
2021-03-20 13:14:45 +0200

    Add tests for Unicafe-maksukortti

--- Laskarit_Exercises/viikko2_week2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
+++ Laskarit_Exercises/viikko2_week2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
@@ -4,11 +4,11 @@ public class Paaohjelma {
 
     public static void main(String[] args) {
         Kassapaate unicafeExactum = new Kassapaate();
-        Maksukortti kortti = new Maksukortti(10000);
-        
+        Maksukortti kortti = new Maksukortti(100);
+        System.out.println(kortti);
         unicafeExactum.syoEdullisesti(kortti);
-        
-        System.out.println( unicafeExactum.edullisiaLounaitaMyyty() );
+
+        System.out.println(unicafeExactum.edullisiaLounaitaMyyty());
         System.out.println(kortti);
     }
 }
--- Laskarit_Exercises/viikko2_week2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ Laskarit_Exercises/viikko2_week2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -1,6 +1,9 @@
 package com.mycompany.unicafe;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,6 +18,56 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void rahanSaldoAlussaOikein() {
+        assertTrue(kortti.saldo() == 10);
+        assertThat(kortti.saldo(), is(equalTo(10)));
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertThat(kortti.saldo(), is(equalTo(20)));
+        kortti.lataaRahaa(1000);
+        assertThat(kortti.saldo(), is(equalTo(1020)));
+    }
+
+    @Test
+    public void rahanOttaminenToimii() {
+        kortti.otaRahaa(2);
+        assertThat(kortti.saldo(), is(equalTo(8)));
+        kortti.otaRahaa(8);
+        assertThat(kortti.saldo(), is(equalTo(0)));
+    }
+
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(2);
+        assertThat(kortti.saldo(), is(equalTo(8)));
+    }
+
+    @Test
+    public void saldoEiMuutuJosRahaaOnLiianVahan() {
+        kortti.otaRahaa(58);
+        assertThat(kortti.saldo(), is(equalTo(10)));
+    }
+
+    @Test
+    public void metodiPalauttaaTrueJosRahatRiittivat() {
+        assertThat(kortti.otaRahaa(2), is(true));
+    }
+
+    @Test
+    public void metodiPalauttaaFalseJosRahatEivatRiittaneet() {
+        assertThat(kortti.otaRahaa(2222), is(false));
+    }
+
+    @Test
+    public void toStringPalauttaaOikein() {
+        Maksukortti k = new Maksukortti(100);
+        assertThat(k.toString(), is(equalTo("saldo: 1.0")));
     }
 }
